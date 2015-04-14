668658a04015762cc62ee70f9d759d475059afb1
osmanjisy <julian@insityou.com>
2015-04-14 22:38:34 +0100

    added tests

--- src/test/java/org/json/CookieTest.java
+++ src/test/java/org/json/CookieTest.java
@@ -18,11 +18,23 @@ public class CookieTest {
 
 		assertEquals("this%09is a tab", Cookie.escape("this\tis a tab"));
 
-		assertEquals("this is a valid cookie name", Cookie.escape("this is a valid cookie name"));
+		assertEquals("this is a valid cookie name?", Cookie.escape("this is a valid cookie name?"));
 
 		assertEquals("%2b", Cookie.escape("+"));
 		assertEquals("%25", Cookie.escape("%"));
 		assertEquals("%3d", Cookie.escape("="));
 		assertEquals("%3b", Cookie.escape(";"));
+	}
+
+	@Test
+	public void testUnescape() {
+		assertEquals("this\tis a tab", Cookie.unescape("this%09is a tab"));
+
+		assertEquals("this is a valid cookie name?", Cookie.unescape("this+is+a+valid+cookie+name?"));
+
+		assertEquals("+", Cookie.unescape("%2b"));
+		assertEquals("%", Cookie.unescape("%25"));
+		assertEquals("=", Cookie.unescape("%3d"));
+		assertEquals(";", Cookie.unescape("%3b"));
 	}
 }
